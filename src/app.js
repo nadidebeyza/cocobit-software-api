86b4dda07dea1bba4910bb074cba6a6456e2ccfc
Nadide Beyza Dokur <nadidebeyzadokur@gmail.com>
2025-06-14 22:40:21 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,2 +1,7 @@
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb', extended: true })); 
+app.use(express.json({ limit: '0' }));
+app.use(express.urlencoded({ limit: '0', extended: true }));
+
+app.use((req, res, next) => {
+  console.log('Request size:', req.headers['content-length']);
+  next();
+}); 
